dab0b9ab513a1d70ed8dda102bb00fd4adc972c7
bubiblomer <bubiblomer@gmail.com>
2020-12-17 01:07:14 -0400

    Added PostgreSQL data for develpment AWS RDS

--- config/database.js
+++ config/database.js
@@ -4,12 +4,30 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        client: "postgres",
+        host: env("TEST_DATABASE_HOST"),
+        port: env.int("TEST_DATABASE_PORT"),
+        database: env("TEST_DATABASE_NAME"),
+        username: env("TEST_DATABASE_USERNAME"),
+        password: env("TEST_DATABASE_PASSWORD"),
+        schema: "public",
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "sqlite",
+//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
